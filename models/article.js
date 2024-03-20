9c39d18961daeca8c23405db64cad31d1783153e
Dillona25 <Dillyb00by49@gmail.com>
2024-03-19 21:14:33 -0500

    feat: Added user model and article schema

---
+++ models/article.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("article", articleSchema);
